b419e45e9762749a1e2e503861f21faea9fd7ac3
idievengine <idievabuali@gmail.com>
2025-12-28 19:11:48 +0000

    Sync repo with live Netlify site (owner.html + supabase-config)

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_PUBLIC_CONFIG = {
+  url: 'https://vfilqtnmoswebeenuvyj.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmaWxxdG5tb3N3ZWJlZW51dnlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY2NDY2NDgsImV4cCI6MjA3MjIyMjY0OH0.h1ZlB7XxAP8EhbI_GSdyU7DRgucOO2BG3psFMbimW6Y',
+};
